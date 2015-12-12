b367b688b0ba01184c2948248296051d0f8b2ed2
Maxime Pinard <maxime.pin@live.fr>
2015-12-12 12:36:23 +0100

    Changing char* order (BaseNIntegerList.c)

--- lib/BaseNIntegerList.c
+++ lib/BaseNIntegerList.c
@@ -63,7 +63,7 @@ BaseNIntegerList RemoveHead(BaseNIntegerList l)
 	{
 		if(l.size == 1)
 		{
-			free(l.heah->value);
+			free(l.head->value);
 			free(l.head);
 			l.head = l.tail = NULL;
 			l.size = 0;
@@ -86,7 +86,7 @@ BaseNIntegerList RemoveTail(BaseNIntegerList l)
 	{		
 		if(l.size == 1)
 		{			
-			free(l.heah->value);
+			free(l.head->value);
 			free(l.head);
 			l.head = l.tail = NULL;
 			l.size = 0;
@@ -119,23 +119,25 @@ unsigned char GetValue(unsigned char c) // make this inline
 unsigned int BaseToInt(char* v, unsigned char base)
 {
 	unsigned int n = 0; // value of v
-	unsigned int temp;
+	unsigned int temp = 1;
 	unsigned int size = strlen(v);
-	unsigned int k = 0;
 	int i = 0;
 
-	i = size - 1; // now i is the index of the last element of v
-
-	while(i >= 0) // from the before last element of v to the first
+	if(size > 0)
 	{
-		temp = 1;
-		for(k = 0; k < (size - i - 1); ++k)
-			temp *= base;
-		// temp = base^(size - i - 1)
-
+		// temp = base^0
 		n += GetValue(v[i]) * temp;
+		++i;
 
-		--i;
+		while(i < size)
+		{
+			temp *= base;
+			// temp = base^i
+
+			n += GetValue(v[i]) * temp;
+
+			++i;
+		}
 	}
 
 	return n;
@@ -143,9 +145,7 @@ unsigned int BaseToInt(char* v, unsigned char base)
 
 char* IntToBase(unsigned int v, unsigned char base)
 {
-	unsigned int size;
 	unsigned int k = 2; // size of the number in the new base: at least '0' and '\0'
-	char ctemp;
 	int i = base;
 	char base_digits[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 	char* w = NULL;
@@ -169,15 +169,6 @@ char* IntToBase(unsigned int v, unsigned char base)
 	// Add '\0' at the end of w
 	w[k] = '\0';
 
-	size = k; // now size is the size of w
-	k = size / 2;
-	for(i = 0; i < k; ++i) // invert w (strongest weight at right -> strongest weight at left)
-	{
-		ctemp = w[i];
-		w[i] = w[size - i - 1];
-		w[size - i - 1] = ctemp;
-	}
-
 	return w;
 }
 
@@ -199,50 +190,52 @@ char* ConvertBinaryToBase(char* v, unsigned char base)
 char* SumBinary(char* a, char* b)
 {
 	char* s = NULL; // the sum
-	int i = strlen(a) - 1; // iteration variable for a
-	int j = strlen(b) - 1; // iteration variable for b
-	unsigned int k = 0;
+	unsigned int i = 0; // iteration variable for a
+	unsigned int a_len = strlen(a);
+	unsigned int j = 0; // iteration variable for b
+	unsigned int b_len = strlen(b);
+	unsigned int k = 0; // iteration variable for s
 	unsigned int sum_len;
 	unsigned char remainder = 0;
 	char binary_digits[2] = {'0', '1'};
 
 	//sum_len = Max(strlen(a), strlen(b)) + 1 (for '\0')
 	if(i > j)
-		sum_len = i + 2;
+		sum_len = strlen(a) + 1;
 	else
-		sum_len = j + 2;
+		sum_len = strlen(b) + 1;
 
-	s = (char*)calloc(sum_len + 1, sizeof(char));
+	s = (char*)calloc(sum_len, sizeof(char));
 
-	// sum each digit of a and b from right to left
-	while(i >= 0 && j >= 0)
+	// sum each digit of a and b from left to right
+	while(i < a_len && j < b_len)
 	{
 		remainder += (a[i] == '1') + (b[j] == '1');
 		s[k] = binary_digits[remainder % 2];
 		remainder /= 2;
 		++k;
-		--i;
-		--j;
+		++i;
+		++j;
 	}
 
-	// if j reached the end (in invert order so the begenning) of b
-	while(i >= 0)
+	// if j reached the end of b
+	while(i < a_len)
 	{
 		remainder += (a[i] == '1');
 		s[k] = binary_digits[remainder % 2];
 		remainder /= 2;
 		++k;
-		--i;
+		++i;
 	}
 
-	// if i reached the end (in invert order so the begenning) of a
-	while(j >= 0)
+	// if i reached the end of a
+	while(j < b_len)
 	{
 		remainder += (b[j] == '1');
 		s[k] = binary_digits[remainder % 2];
 		remainder /= 2;
 		++k;
-		--j;
+		++j;
 	}
 
 	// if there is a reminder at the end of the sum
@@ -256,67 +249,58 @@ char* SumBinary(char* a, char* b)
 	// Add '\0' at the end of s
 	s[k] = '\0';
 
-	// invert s (strongest weight at right -> strongest weight at left)
-	unsigned int size = k;
-	char ctemp;
-	k /= 2;
-	for(i = 0; i < k; ++i)
-	{
-		ctemp = s[i];
-		s[i] = s[size - i - 1];
-		s[size - i - 1] = ctemp;
-	}
-
 	return s;
 }
 
 char* SumBase(char* a, char* b, unsigned char base)
 {
 	char* s = NULL; // the sum
-	int i = strlen(a) - 1; // iteration variable for a
-	int j = strlen(b) - 1; // iteration variable for b
-	unsigned int k = 0;
+	unsigned int i = 0; // iteration variable for a
+	unsigned int a_len = strlen(a);
+	unsigned int j = 0; // iteration variable for b
+	unsigned int b_len = strlen(b);
+	unsigned int k = 0; // iteration variable for s
 	unsigned int sum_len;
 	unsigned char remainder = 0;
 	char base_digits[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
 	//sum_len = Max(strlen(a), strlen(b)) + 1 (for '\0')
 	if(i > j)
-		sum_len = i + 2;
+		sum_len = strlen(a) + 1;
 	else
-		sum_len = j + 2;
+		sum_len = strlen(b) + 1;
 
-	s = (char*)calloc(sum_len + 1, sizeof(char));
+	s = (char*)calloc(sum_len, sizeof(char));
 
-	// sum each digit of a and b from right to left
-	while(i >= 0 && j >= 0)
+	// sum each digit of a and b from left to right
+	while(i < a_len && j < b_len)
 	{
 		remainder += GetValue(a[i]) + GetValue(b[j]);
 		s[k] = base_digits[remainder % base];
 		remainder /= base;
 		++k;
-		--i;
-		--j;
+		++i;
+		++j;
 	}
 
-	// if j reached the end (in invert order so the begenning) of b
-	while(i >= 0)
+	// if j reached the end of b
+	while(i < a_len)
 	{
 		remainder += GetValue(a[i]);
 		s[k] = base_digits[remainder % base];
 		remainder /= base;
 		++k;
-		--i;
+		++i;
 	}
 
-	// if i reached the end (in invert order so the begenning) of a
-	while(j >= 0)
+	// if i reached the end of a
+	while(j < b_len)
 	{
 		remainder += GetValue(b[j]);
 		s[k] = base_digits[remainder % base];
 		remainder /= base;
 		++k;
-		--j;
+		++j;
 	}
 
 	// if there is a reminder at the end of the sum
@@ -329,17 +313,6 @@ char* SumBase(char* a, char* b, unsigned char base)
 
 	// Add '\0' at the end of s
 	s[k] = '\0';
-
-	// invert s (strongest weight at right -> strongest weight at left)
-	unsigned int size = k;
-	char ctemp;
-	k /= 2;
-	for(i = 0; i < k; ++i)
-	{
-		ctemp = s[i];
-		s[i] = s[size - i - 1];
-		s[size - i - 1] = ctemp;
-	}
 
 	return s;
 }
